135f08f7ce8da31deb65f1dc46dd947c0c5f7e81
Qiuzero <1581769275@qq.com>
2020-09-01 21:02:00 +0800

    完成部门管理模块

--- js/departmentadd.js
+++ js/departmentadd.js
@@ -1,6 +1,65 @@
-$(function(){
+// $(function(){
+//     function checkname() {
+//         let val = $(".departmentname").val().trim();
+//         if (val.length === 0) {
+//             $(".spanname").html('必填项')
+//             return false;
+//         }
+//         if (!/^[\u4e00-\u9fa5]{2,10}$/.test(val)) {
+//             $(".spanname").html('名字必须是2~10个汉字')
+//             return false;
+//         }
+//         $(".spanname").html('')
+//         return true
+//     }
+//     $(".departmentname").blur(checkname)
+//     $(".submit").click(async function () {
+//         let params = {
+//             name: $('.departmentname').val().trim(),
+//             desc: $(".departmentdesc").val().trim(),
+//         }
+//         console.log(params)
+//         //实现新增功能
+//         let result = await axios.post("/departmentartment/add", params)
+//         if (result.code === 0) {
+//             alert("添加成功");
+//             window.location.href = "departmentartmentlist.html";
+//             return
+//         }
+//         alert("添加失败~稍后再试")
+//     })
+// })
+$(function () {
+    let departmentId = null;
+    console.log(window,location.href);
+    let params = window.location.href.queryURLParams();
+    console.log(params);
+    //点击编辑进到此页面有id, 点击添加进到此页面没有id
+    if (params.hasOwnProperty("id")) {
+        departmentId = params.id;
+        getBaseInfo();
+    }    
+    async function getBaseInfo() {
+        let result = await axios.get("/department/info", {
+            params: {
+                departmentId
+            }
+        })
+        if (result.code === 0) {
+            result = result.data;
+            //实现数据的回显
+            $(".departmentname").val(result.name);
+            $(".departmentdesc").val(result.desc);
+            return;
+
+        }
+        alert("稍后再试")
+        departmentId = null;
+    }
+    //数据校验
+    //用户名验证
     function checkname() {
-        let val = $(".depname").val().trim();
+        let val = $(".departmentname").val().trim();
         if (val.length === 0) {
             $(".spanname").html('必填项')
             return false;
@@ -12,17 +71,37 @@ $(function(){
         $(".spanname").html('')
         return true
     }
-    $(".depname").blur(checkname)
+    $(".departmentname").blur(checkname)
     $(".submit").click(async function () {
-        let params = {
-            name: $('.depname').val().trim(),
-            desc: $(".depdesc").val().trim(),
+        if (!checkname()) {
+            alert('你填写的数据不合法')
+            return;
         }
-        console.log(params)
+        //校验通过获取用户输入的数据
+        let params = {
+            name: $('.departmentname').val().trim(),
+            desc: $(".departmentdesc").val().trim(),
+        }
+        //console.log(params)
+
+        //判断是编辑还是新增
+        if (departmentId) {
+            //实现编辑功能
+            params.departmentId = departmentId;
+            let result = await axios.post("/department/update", params)
+            if (result.code === 0) {
+                alert("编辑成功")
+                window.location.href = 'departmentlist.html'
+                return;
+            }
+            alert("编辑失败，稍后再试")
+            return;
+        }
+
         //实现新增功能
         let result = await axios.post("/department/add", params)
         if (result.code === 0) {
-            alert("添加成功");
+            // alert("添加成功");
             window.location.href = "departmentlist.html";
             return
         }
--- js/departmentlist.js
+++ js/departmentlist.js
@@ -14,7 +14,7 @@ $(function () {
                 <td class="w10">${id}</td>
                 <td class="w20">${name}</td>
                 <td class="w40">${desc}</td>
-                <td class="w20">
+                <td class="w20" departmentId='${id}'>
                     <a href="javascript:;">编辑</a>
                     <a href="javascript:;">删除</a>
                 </td>
@@ -25,34 +25,33 @@ $(function () {
 
     }
     delegate();
+
     function delegate() {
         $("tbody").on('click', 'a', e => {
             let target = e.target,
                 tag = target.tagName,
                 text = target.innerHTML.trim();
-            if (tag === 'A') {
-                let customerId = $(target).parent().attr("customerId");
-                if (text === '编辑') {
-                    window.location.href = `customeradd.html?id=${customerId}`
+            if(tag==='A'){
+                let departmentId=$(target).parent().attr("departmentId");
+                if(text==='编辑'){
+                    window.location.href=`departmentadd.html?id=${departmentId}`
                     return;
-
+                    
                 }
-                if (text === '删除') {
+                if(text==='删除'){
                     //console.log('删除');
-                    let flag = confirm("你确定要删除此用户吗")
-                    if (!flag) return;
-                    let result = axios.get("/customer/delete", {
-                        params: {
-                            customerId
-                        }
+                    let flag=confirm("你确定要删除此用户吗")
+                    if(!flag) return;
+                    let result=axios.get("/department/delete",{
+                        params:{departmentId}
                     })
-                    if (result.code === 0) {
+                    if(result.code===0){
                         alert("删除成功")
                         $(target).parent().parent().remove();
                         return;
                     }
                     return;
-
+                    
                 }
             }
         })
